374d4d9f28b3a827ab36d7affa5b7b1c63b957ce
Yujie Wang <archiyujie@gmail.com>
2019-07-03 20:45:44 +0800

    Scripts

---
+++ Scripts/IfHitTag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfHitTag : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject trashElement;
+    public GameObject trashCan;
+    public GameObject prefab;
+    public Rigidbody rb;
+
+    public float speed;
+
+    void Awake ()
+    {
+
+
+    }
+
+        void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject.CompareTag("TrashCan"))
+            {
+            rb.isKinematic = false;
+
+            Destroy(trashElement);
+
+            for (int i = 0; i < 1; i++)
+                Instantiate(prefab, new Vector3(i * -0.06f, 1.9704f, 10.9f), Quaternion.identity);
+
+            trashCan.transform.Rotate(Vector3.up * speed * 360);
+
+            Debug.Log("hit");
+
+        }
+
+        }
+
+}
---
+++ Scripts/ManagerTouch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerTouch : MonoBehaviour
+{
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // input 01 - if trash element is touched [repeat this for ultiple elements ]
+
+
+            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "News")
+            {
+                if (hit.collider != null)
+                {
+
+                  //something 
+                }
+            }
+
+            // input 02 - if trash element collides the right trashcan [repeat this for ultiple elements ]
+
+          
+
+
+
+
+    
+
+
+            // condition to create is - if after 10 second there is no collision - loose - reset 
+
+        }
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Enemy"))
+        {
+           //
+        }
+
+   
+    }
+}
